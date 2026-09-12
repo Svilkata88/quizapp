8e3bf1f6d59978cd510eb6e9689510f93f3e6f66
Svilen Ivanov <ivanovsvilen88@gmail.com>
2026-09-12 14:37:52 +0300

    backend; updated get_daily_quizzes_summary import

--- backend/quizapi/daily_quiz/urls.py
+++ backend/quizapi/daily_quiz/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_current_daily_topic_and_user_daily_quiz, get_daily_quiz_questions, get_daily_quiz_summary, update_daily_quiz_after_game
+from .views import get_current_daily_topic_and_user_daily_quiz, get_daily_quiz_questions, get_daily_quizzes_summary, update_daily_quiz_after_game
 
 
 
@@ -7,5 +7,5 @@ urlpatterns = [
     path('get_daily_questions/', get_daily_quiz_questions, name='get_daily_quiz_questions'),
     path('daily-topic/', get_current_daily_topic_and_user_daily_quiz, name='get_current_daily_topic_and_user_daily_quiz'),
     path('update_daily_quiz/', update_daily_quiz_after_game, name='update_daily_quiz_after_game'),
-    path('daily_quiz_summary/', get_daily_quiz_summary, name='get_daily_quiz_summary'),
+    path('daily_quiz_summary/', get_daily_quizzes_summary, name='get_daily_quizzes_summary'),
 ]
